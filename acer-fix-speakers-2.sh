f93a1b110db813b32dc77ee2758a35bb62b64f85
thearchhero <pro_linux_nerd@disroot.org>
2025-01-26 20:16:09 +0200

    Add files via upload

---
+++ acer-fix-speakers-2.sh
@@ -0,0 +1,9 @@
+amixer sset Master unmute
+amixer sset Speaker unmute
+amixer sset Headphone unmute
+amixer sset Master 100%
+amixer sset Speaker 100%
+amixer sset Headphone 100%
+clear
+echo Testing speakers...
+echo Done! Your speakers should now work.
